7e8fbc1e63bb68826fb77d780fb1ff85db7aa051
PierreOF <marcos.pierre@upe.br>
2023-10-11 12:06:30 -0300

    feat requisicao post funcional

--- src/main/webapp/script/script.js
+++ src/main/webapp/script/script.js
@@ -1,6 +1,36 @@
 
+function fazerRequisicaoPOST() {
+    console.log("fazer requisicao");
+    const url = 'http://localhost:8080/Projeto-weather-forecast/WeatherAcess';
+    const data = {
+        "lat": 10,
+        "lon": 10
+        // Adicione mais dados conforme necessário
+    };
 
-const express = require("express");
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    };
+
+    fetch(url, requestOptions)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erro na requisição');
+
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Catch Erro na requisição:', error);
+        });
+}
 
 function atualizarMapa(novaLatitude, novaLongitude) {
     let mapa = document.getElementById("mapaVisualizer");
@@ -13,34 +43,4 @@ function atualizarMapa(novaLatitude, novaLongitude) {
     mapa.src = link;
 }
 
-function fazerRequisicaoPOST() {
-    const url = 'https://localhost:8080/Projeto-weather-forecast/WeatherAcess';
-    const data = {
-        "lat": 10,
-        "lon": 10
-        // Adicione mais dados conforme necessário
-    };
 
-    const requestOptions = {
-        mode: 'no-cors',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-    };
-
-    fetch(url, requestOptions)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Erro na requisição');
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
-        })
-        .catch(error => {
-            console.error('Erro na requisição:', error);
-        });
-}
